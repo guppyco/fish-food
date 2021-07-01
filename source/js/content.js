27c17d6fbfa468b064f35df5d0910e7ae1380372
AuTN <autn@greenglobal.vn>
2021-07-01 18:45:15 +0700

    Update lint

--- source/js/content.js
+++ source/js/content.js
@@ -122,40 +122,40 @@ function askToLoginHtml() {
   chrome.storage.local.get(['isAskedLoginHtml'], response => {
     // Show notification one time per day
     if (!response.isAskedLoginHtml || response.isAskedLoginHtml !== today) {
-      let div = document.createElement('div')
+      const div = document.createElement('div')
       div.className = 'guppy-ask-to-login-banner'
-      let div2 = document.createElement('div')
+      const div2 = document.createElement('div')
       div2.className = 'main'
-      let div3 = document.createElement('div')
+      const div3 = document.createElement('div')
       div3.className = 'text'
-      let message = document.createElement('a')
+      const message = document.createElement('a')
       message.className = 'message'
       message.textContent = 'You aren\'t logged into your Guppy account - ' +
         'You\'ll be missing out on cash back rewards until you login. ' +
         'Please click to login to Guppy'
-      message.onclick = () => {
+      message.addEventListener('click', () => {
         chrome.runtime.sendMessage({
-            message: 'openLoginForm'
-          }
-        )
-      }
-      let remove = document.createElement('a')
+          message: 'openLoginForm'
+        })
+      })
+
+      const remove = document.createElement('a')
       remove.className = 'remove'
       remove.textContent = 'x'
-      remove.onclick = () => {
+      remove.addEventListener('click', () => {
         div.style.display = 'none'
 
         chrome.storage.local.set({
           isAskedLoginHtml: today
         })
-      }
+      })
 
-      div3.appendChild(message)
-      div2.appendChild(div3)
-      div2.appendChild(remove)
-      div.appendChild(div2)
+      div3.append(message)
+      div2.append(div3)
+      div2.append(remove)
+      div.append(div2)
 
-      document.body.appendChild(div)
+      document.body.append(div)
     }
   })
 }
